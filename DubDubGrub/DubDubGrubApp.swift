b77655c8a750ecf181d49e00d6dff5033b6a09e3
codedubu <mccaine.river@gmail.com>
2022-06-27 11:22:41 -0700

    Initial commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by River McCaine on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
