93ef21ea0219e040d4bf2c06a85c48b4c0889361
codedubu <mccaine.river@gmail.com>
2022-06-28 20:57:10 -0700

    Adds LocationMapView's ViewModel. Refactors code to accomodate for view now being changed by view model.

---
+++ DubDubGrub/Screens/LocationMapView/LocationMapViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  LocationMapViewModel.swift
+//  DubDubGrub
+//
+//  Created by River McCaine on 6/28/22.
+//
+
+import SwiftUI
+import MapKit
+
+final class LocationMapViewModel: ObservableObject {
+    
+    @Published var locations: [DDGLocation] = []
+    @Published var alertItem: AlertItem?
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
+                                               span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
+    func getLocations() {
+        CloudKitManager.getLocations { [self] result in
+            DispatchQueue.main.async {
+                switch result {
+                    
+                case .success(let locations):
+                    self.locations = locations
+                case .failure(_):
+                    self.alertItem = AlertContext.unableToGetLocations
+                }
+            }
+        }
+    }
+}
